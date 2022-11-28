c50c506ed8c97cc3153ca0a5e14032ee7998cba7
colorodriguezf <colitorodriguez97@gmail.com>
2022-11-28 19:41:07 -0300

    solucion

--- TP2 y TP4/js/header-sticky.js
+++ TP2 y TP4/js/header-sticky.js
@@ -1,25 +1,12 @@
 var header = document.getElementById('sticky');
 
 
-onScroll = () => {
-  var scrolledPage = Math.round(window.pageYOffset);
-  if(scrolledPage > 10) {
-    header.classList.add('sticky');
-  } else {
-    header.classList.remove('sticky');
-  }
-}
-
-
-document.addEventListener('scroll', onScroll);
-
-
 
     var logo = document.querySelector('.nav-logo');
     let prevY = window.scrollY; //Posicion del scroll al inicio
     //Detecto la posicion del scroll
     window.addEventListener('scroll', function(){
-        if(prevY > window.scrollY) { //si sube
+        if(prevY >= window.scrollY +10) { //si sube
           header.classList.remove('reduce');
         } else { //baja el scroll
           header.classList.add('reduce');
