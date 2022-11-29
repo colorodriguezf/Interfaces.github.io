2acca625ff868d6186350ec3ae5bbc407a452a09
colorodriguezf <colitorodriguez97@gmail.com>
2022-11-28 21:03:08 -0300

    punto 9

--- TP2 y TP4/js/header-sticky.js
+++ TP2 y TP4/js/header-sticky.js
@@ -14,3 +14,45 @@ var header = document.getElementById('sticky');
         prevY = window.scrollY; //seteo la posicion del scroll
     })
     
+
+
+
+
+
+
+
+
+
+
+
+
+    //Punto 9, separación entre el título y personajes
+    var titulo = document.querySelector("#titulo-efecto");
+    titulo.style.opacity = 0.1;
+    var distance=0;
+    var movimiento = 100
+    titulo.style.transform = `translateY(-${movimiento*2}px)`;
+
+window.addEventListener("scroll", function(){
+    var windowHeight = window.innerHeight;
+    var elementTop = titulo.getBoundingClientRect().top;
+    var elementVisible = 400;
+    var distanceNow = window.scrollY;
+
+    if(elementTop < (windowHeight - elementVisible) && titulo.style.opacity<1 && distanceNow>distance && movimiento>0){
+        titulo.style.opacity = titulo.style.opacity*1.2;
+        titulo.style.transform = `translateY(-${movimiento*2}px)`;
+        movimiento = movimiento -2;
+    }else if (elementTop > (windowHeight - elementVisible) && distanceNow<distance && titulo.style.opacity>0.1){
+        titulo.style.opacity = titulo.style.opacity*0.9;
+        titulo.style.transform = `translateY(-${movimiento*2}px)`;
+        movimiento = movimiento +2;
+    }
+
+    if (movimiento>30){
+        movimiento = 30;
+    }
+
+    distance=distanceNow;
+
+});
