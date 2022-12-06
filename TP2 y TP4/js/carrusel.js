ca10f0fece352846b30a771c08738f11f190701f
colorodriguezf <colitorodriguez97@gmail.com>
2022-12-05 21:12:48 -0300

    Animacion carrousel, y cards (se acerca titulo e imagen)

--- TP2 y TP4/js/carrusel.js
+++ TP2 y TP4/js/carrusel.js
@@ -55,3 +55,62 @@ let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
         track.style.left = `0px`
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    console.log(n);
+   let fade = document.querySelectorAll(".fade");
+   for (let i = 0; i <fade.length; i++) {
+    if(n == 1) {
+        fade[i].classList.remove("izquierda");
+        fade[i].classList.add("derecha");
+    }
+    else if(n == -1) {
+        console.log("ENTRO");
+        fade[i].classList.remove("derecha");
+       fade[i].classList.add("izquierda");
+    }
+   }
+  showSlides(slideIndex += n);
+}
+
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
